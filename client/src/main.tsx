8a5b61dbdaf8c7caaf9cf43f02e6e7fed7feece0
DanishSjjd <danishsjjad@gmail.com>
2023-12-10 23:31:30 +0500

    feat: example env, service-account.json

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,9 +5,7 @@ import "./index.css"
 import { loadStripe } from "@stripe/stripe-js"
 import { Elements } from "@stripe/react-stripe-js"
 
-const stripePromise = loadStripe(
-  "pk_test_51Nz0iwCwDMOWi2D3D16hVwf9KPys9dbdAOqE5zeuQnbnN8SzzIKbo0YNuneczmnncm3HK12Y6Jc21GBjKifs4DgG00mf10jXLg"
-)
+const stripePromise = loadStripe(import.meta.VITE_STRIPE_PUBLIC_KEY)
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
